9d87e5ee7e1aaf17dd08cd284d6362ab6312ab89
Ongki Herlambang <meepomodeon@gmail.com>
2019-12-20 17:42:00 +0700

    add carousel

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -60,3 +60,101 @@
 
 	</div>
 </div>
+
+	<!-- Carousel
+	================================================== -->
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#myCarousel" data-slide-to="1"></li>
+		<li data-target="#myCarousel" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner" role="listbox">
+
+      <div class="item active" style="background-image:url(./src/img/carousel/ex_slider_01.jpg)">
+				<div class="container">
+					<div class="carousel-caption">
+						<h4>Our Mission</h4>
+						<h1>A City for All</h1>
+						<p>Improving participatory planning and budgeting processes by making information available to citizens.</p>
+						<p><a class="btn btn-lg btn-default" href="about-us#our-mission" role="button">Learn about our mission</a></p>
+					</div>
+				</div>
+      </div>
+
+			<!-- <div class="item" style="background-image:url(./src/img/carousel/slider_another-city.jpg)">
+				<div class="container">
+					<div class="carousel-caption">
+						<h4>Latest News</h4>
+						<h1>Another City is Possible</h1>
+						<p>The 2nd Urban Social Forum was hosted and organized by Kota Kita on December 20th, 2014 in Solo.</p>
+						<p><a class="btn btn-lg btn-default" href="http://kotakita-journal.tumblr.com/post/109590341832/another-city-is-possible" role="button">Read more</a></p>
+					</div>
+				</div>
+      </div> -->
+
+			<!-- <div class="item" style="background-image:url(./src/img/feature/development-boom/banner-dark.jpg)">
+				<div class="container">
+					<div class="carousel-caption">
+						<h4>Feature</h4>
+						<h1>Indonesia Development Boom Ignores Community Voices</h1>
+						<p>The communities most affected are having little say in how their environment should be shaped</p>
+						<p><a class="btn btn-lg btn-default" href="feature-development-boom" role="button">Read more</a></p>
+					</div>
+				</div>
+      </div> -->
+
+      <div class="item" style="background-image:url('https://res.cloudinary.com/vasilenka/image/upload/v1533597764/kotakita/featured/participatory-budgetting.jpg')">
+				<div class="container">
+					<div class="carousel-caption">
+						<h4>Library</h4>
+						<h1>Participatory Budgeting in Indonesia: Past, Present, and Future</h1>
+						<p><small>Kota Kita reflects on the history and evolution of participatory budgeting in Indonesia.</small></p>
+						<p><a class="btn btn-lg btn-default" href="./publications-docs/participatory_budgeting_asia_russia_final.pdf" role="button" target="_blank">Read more</a></p>
+					</div>
+				</div>
+      </div>
+
+      <div class="item" style="background-image:url('https://res.cloudinary.com/vasilenka/image/upload/v1533597767/kotakita/featured/disability.jpg')">
+				<div class="container">
+					<div class="carousel-caption">
+						<h4>Video</h4>
+						<h1>Participatory data collection for disability-inclusive city: Solo City</h1>
+						<p><small>Participatory process enables mapping of various key determinants of exclusion and inclusion.</small></p>
+						<p><a class="btn btn-lg btn-default" target="_blank" href="https://www.youtube.com/watch?v=GVWqvL1XKtI&t=14s" role="button">Watch here</a></p>
+					</div>
+				</div>
+      </div>
+
+			<!-- <div class="item" style="background-image:url(./src/img/feature/mongolia-planning/banner-dark.jpg)">
+				<div class="container">
+					<div class="carousel-caption">
+						<h4>Feature</h4>
+						<h1>Participatory Urban Planning Takes Hold in Mongolia</h1>
+						<p>Mapping enables citizens to interact in planning, budgeting, &amp; urban development decisions</p>
+						<p><a class="btn btn-lg btn-default" href="feature-mongolia-planning" role="button">Read more</a></p>
+					</div>
+				</div>
+			</div> -->
+
+			<!-- <div class="item" style="background-image:url(./src/img/feature/water-water/banner.jpg)">
+				<div class="container">
+					<div class="carousel-caption">
+						<h4>Feature</h4>
+						<h1>Water, water everywhere but not a drop to drink</h1>
+						<p>Poor distribution and poor infrastructure makes it very difficult to access water</p>
+						<p><a class="btn btn-lg btn-default" href="feature-water-water" role="button">Read more</a></p>
+					</div>
+				</div>
+			</div> -->
+		</div>
+		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		<span class="sr-only">Next</span>
+		</a>
+	</div><!-- /.carousel -->
